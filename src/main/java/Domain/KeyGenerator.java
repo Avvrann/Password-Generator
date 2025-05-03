4b91f2649dff962acc41f52952d2906584f1d24f
avrann <ahbr4936@gamil.com>
2025-05-03 12:19:11 -0600

    Se agrega la funcionalidad del generador de contrasenas

--- src/main/java/Domain/KeyGenerator.java
+++ src/main/java/Domain/KeyGenerator.java
@@ -1,6 +1,5 @@
 package Domain;
 
-import java.net.PortUnreachableException;
 import java.util.ArrayList;
 
 public class KeyGenerator {
@@ -11,41 +10,26 @@ public class KeyGenerator {
 
     public static ArrayList<String> password = new ArrayList<>();
 
-    private int lenghtPassword= 10;
-    private boolean useSpecialCharacters;
-    private boolean useUpperCaseCharaters;
-    private boolean useLowerCaseCharacters;
-    private boolean useNumbers;
+    public static int lenghtPassword;
+    public static boolean useSpecialCharacters;
+    public static boolean useUpperCaseCharaters;
+    public static boolean useLowerCaseCharacters;
+    public static boolean useNumbers;
 
     public KeyGenerator(){}
 
-    public KeyGenerator(boolean useSpecialCharacters,
-                        boolean useNumbers,
-                        boolean useLowerCase,
-                        boolean useUpperCase,
-                        int lenghtPassword){
+    public static void setUPPER(){useUpperCaseCharaters = true;}
+    public static void setLOWER(){useLowerCaseCharacters = true;}
+    public static void setSPECIALS(){useSpecialCharacters = true;}
+    public static void setDIGITS (){useNumbers = true;}
 
-        this.useNumbers = useNumbers;
-        this.lenghtPassword = lenghtPassword;
-        this.useLowerCaseCharacters = useLowerCase;
-        this.useUpperCaseCharaters = useUpperCase;
-        this.useSpecialCharacters = useSpecialCharacters;
 
-        validateOneTipeOfCharacters();
-    }
+    public static void disableUPPER(){useUpperCaseCharaters = false;}
+    public static void disableLOWER(){useLowerCaseCharacters = false;}
+    public static void disableSPECIALS(){useSpecialCharacters = false;}
+    public static void disableDIGITS (){useNumbers = false;}
 
-    public void setUPPER(){
-        this.useUpperCaseCharaters = true;
-    }
-    public void setLOWER(){
-        this.useLowerCaseCharacters = true;
-    }
-    public void setSPECIALS(){
-        this.useSpecialCharacters = true;
-    }
-    public void setDIGITS (){
-        this.useNumbers = true;
-    }
+    public void setLenghtPasswordP(int cantiadDigitos){lenghtPassword = cantiadDigitos;}
 
     public void validateOneTipeOfCharacters(){
         if (!(useSpecialCharacters || useNumbers || useLowerCaseCharacters || useUpperCaseCharaters)){
@@ -55,27 +39,45 @@ public class KeyGenerator {
 
     public StringBuilder appendTipesOfCharacters(){
         StringBuilder charsAppend = new StringBuilder();
+        charsAppend.delete(0,charsAppend.length());
+        System.out.println("=========================================");
+        System.out.println(charsAppend);
+        System.out.println("=========================================");
         if (useUpperCaseCharaters) charsAppend.append(UPPER);
+        System.out.println("estado de las mayusculas : " + useUpperCaseCharaters);
         if (useLowerCaseCharacters) charsAppend.append(LOWER);
+        System.out.println("estado de las minusculas : " + useLowerCaseCharacters);
         if (useSpecialCharacters) charsAppend.append(SPECIALS);
+        System.out.println("estado de los caracteres especiales : " + useSpecialCharacters);
         if (useNumbers) charsAppend.append(DIGITS);
+        System.out.println("estado de los numeros : " + useNumbers);
         return  charsAppend;
     }
 
     public String generatePassword(){
-        StringBuilder allChars = appendTipesOfCharacters();
 
-        for (int n = 0; n < lenghtPassword; n ++){
-            int ramdomIndex = (int) (Math.random() * allChars.length());
-            char caracterRamdom = allChars.charAt(ramdomIndex);
-            password.add(String.valueOf(caracterRamdom));
-        }
-        StringBuilder passworString = new StringBuilder();
+        try {
+            StringBuilder allChars = appendTipesOfCharacters();
 
-        for (String element : password){
-            passworString.append(element);
+            for (int n = 0; n < lenghtPassword; n ++){
+                int ramdomIndex = (int) (Math.random() * allChars.length());
+                char caracterRamdom = allChars.charAt(ramdomIndex);
+                password.add(String.valueOf(caracterRamdom));
+            }
+            StringBuilder passworString = new StringBuilder();
+
+            for (String element : password){
+                passworString.append(element);
+            }
+
+            password.clear();
+            return passworString.toString();
+
+
+        }catch (Exception e){
+            System.out.println("faltan caracteres");
         }
-        return passworString.toString();
+        return null;
     }
 }
 
--- src/main/java/com/example/passwordgenerator/PasswordGeneratorController.java
+++ src/main/java/com/example/passwordgenerator/PasswordGeneratorController.java
@@ -4,42 +4,79 @@ import Domain.KeyGenerator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PasswordGeneratorController implements Initializable {
+
+    @FXML
+    TextArea areaInt = new TextArea();
+    @FXML
     public TextArea areaDeTexto;
+
     KeyGenerator password = new KeyGenerator();
 
     @FXML
-    protected void btnUpper(ActionEvent event) {
-        password.setUPPER();
-    }
+    private CheckBox setUpperCase;
+    @FXML
+    private CheckBox setLowerCase;
+    @FXML
+    private CheckBox setSpecials;
+    @FXML
+    private CheckBox setDigits;
+
+    //Botones para setear los caracteres a usar en la contrasena.
 
     @FXML
     protected void btnGenerar(ActionEvent event) {
         areaDeTexto.clear();
         areaDeTexto.setText(password.generatePassword());
+        System.out.println(password.generatePassword());
     }
-    @FXML
-    protected void btnMinusculas(ActionEvent event) {
-        password.setLOWER();
-    }
-    @FXML
-    protected void btnDigitos(ActionEvent event) {
-        password.setDIGITS();
-    }
-
-    @FXML
-    protected void btnCaracteresEspeciales(ActionEvent event) {
-        password.setSPECIALS();
-    }
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        setUpperCase.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (!isNowSelected) {
+                KeyGenerator.disableUPPER();
+
+                System.out.println("Se desabilitan las mayusculas");
+            }else {
+                KeyGenerator.setUPPER();
+            }
+        });
+        setSpecials.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (!isNowSelected) {
+                KeyGenerator.disableSPECIALS();
+                isNowSelected = false;
+                System.out.println("se desabilitan los catacteres especiales");
+            }else {KeyGenerator.setSPECIALS();}
+
+        });
+        setLowerCase.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (!isNowSelected) {
+                KeyGenerator.disableLOWER();
+
+                System.out.println("se desabilitan las minusculas");
+            }else {
+                KeyGenerator.setLOWER();
+            }
+        });
+        setDigits.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (!isNowSelected) {
+                KeyGenerator.disableDIGITS();
+                System.out.println("se desabilitan los digitos");
+            }else {KeyGenerator.setDIGITS();}
+        });
+    }
+
+    public void btnAceptar(ActionEvent event) {
+        int cantiadDigitos = Integer.parseInt(areaInt.getText());
+        password.setLenghtPasswordP(cantiadDigitos);
     }
 }
